f5b853dde99fc7cd85ef07901fcc0de00c8beec3
Haley Proctor <hproctor021@gmail.com>
2020-07-30 13:37:33 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,8 +1,8 @@
-def loop_message_five_times(string)
+def loop_message_five_times(message)
   counter = 0
   
   while counter < 5 do
-    puts string[counter]
+    puts message[counter]
     counter += 1
   end
 end
