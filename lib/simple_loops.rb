125f4e2baa7ae5d9c9901adac38f0c31f2e979c1
Haley Proctor <hproctor021@gmail.com>
2020-07-30 13:53:15 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -25,5 +25,11 @@ def output_array(array)
   end
 end
 
-def return_string_array
+def return_string_array(array)
+  counter = 0
+  
+  while counter < array.length do
+    return array
+    counter += 1
+  end
 end
